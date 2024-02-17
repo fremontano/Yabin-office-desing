1cf3004f9846859045cf6fe802aa2fd7d97ccb32
freyder_montano <fremontano24@gmail.com>
2024-02-16 23:28:17 -0300

    Creando la seccion de intro

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function userScroll() {
-    const navbar = document.querySelector('.navbar');
+    const counters = document.querySelectorAll('.counter');
 
     window.addEventListener('scroll', () => {
 
@@ -12,4 +12,28 @@ function userScroll() {
 };
 
 
+// stast 
+function incrementStats() {
+    const counters = document.querySelectorAll('.counter');
+
+    counters.forEach((counter) => {
+        counter.innerText = 0;
+
+        const updateCounter = () =>{
+            const target = +counter.getAttribute('data-target');
+            const c = +counter.innerText;
+
+            const increment = target / 200;
+            if(c < target){
+              counter.innerText = Math.ceil(c + increment);
+                setTimeout(updateCounter, 1);
+            }else{
+                counter.innerText  = target;
+            }
+        };
+
+        updateCounter();
+    });
+}
 document.addEventListener('DOMContentLoaded', userScroll());
+document.addEventListener('DOMContentLoaded', incrementStats);
